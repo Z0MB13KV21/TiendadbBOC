f4f93056a7417bc2a77f9145cc7f380c8a6f90d4
Carlos Obando <136123266+Z0MB13KV21@users.noreply.github.com>
2024-08-12 20:12:37 -0600

    Delete tiendadb directory

--- tiendadb/public/js/log.js
+++
@@ -1,6 +0,0 @@
-function login() {
-    // Obtener los valores ingresados por el usuario
-    var usuario = document.getElementById("correoInicioSesion").value;
-        localStorage.setItem("nombreUsuario", usuario);
-}
-
